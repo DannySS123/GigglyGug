d27167478fbc43611f4744b2bf56d54cd90cec6b
Zsombor Kászonyi <kaszonyirelli@gmail.com>
2022-10-20 15:33:22 +0200

    PathFindingEnemy

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -2,19 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Respawn : MonoBehaviour
-{
+public class Respawn : MonoBehaviour {
     public GameObject respawnPosition;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(transform.position.y < -10){
+    void Update() {
+        if (transform.position.y < -10) {
             transform.position = respawnPosition.transform.position;
         }
     }
