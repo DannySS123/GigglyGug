2131df56220a836c4dd734daf0c84a4cb64b74e7
DannyS123 <szdani2012@gmail.com>
2022-10-21 13:54:45 +0200

    Swording kinda done

--- Assets/Scripts/Player/SwordAttack.cs
+++ Assets/Scripts/Player/SwordAttack.cs
@@ -5,28 +5,43 @@ using UnityEngine;
 public class SwordAttack : MonoBehaviour
 {
     public Animator animator;
+    public GameObject sword;
     public Transform attackPoint;
     public float attackRange = 0.5f;
+    public float attackTime = 0.5f;
     public LayerMask enemyLayers;
+    private bool canAttack = true;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && canAttack) {
             Attack();
         }
     }
 
     void Attack() {
+        canAttack = false;
+        sword.SetActive(true);
         animator.SetTrigger("SwordAttack");
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
+        Invoke("DisableSword", 0.3f);
+        Invoke("AllowAttack", attackTime);
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
         foreach (Collider2D enemy in hitEnemies) {
             Debug.Log("HIT");
             Destroy(enemy.gameObject);
         }
     }
 
+    void DisableSword() {
+        sword.SetActive(false);
+    }
+
+    void AllowAttack() {
+        canAttack = true;
+    }
     private void OnDrawGizmosSelected() {
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
