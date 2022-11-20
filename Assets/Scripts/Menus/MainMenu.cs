684f4804fc5d5045f500e0380a14587919ab5e5e
Zsombor Kászonyi <kaszonyirelli@gmail.com>
2022-11-20 14:01:50 +0100

    PathFindingAI fix and Level organizing
    (Added A* to all levels)

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame() 
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame() 
